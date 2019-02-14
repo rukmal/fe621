95385bfb957fefb9f03f58de407b730b58108f65
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-02-14 13:30:23 -0500

    Changed termination condition of newton method solver for better performance with option datasets

--- fe621/optimization/newton.py
+++ fe621/optimization/newton.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def newtonSolver(f: Callable, f_prime: Callable, guess: float,
-                 tol: float=10e-6) -> float:
+                 tol: float=10e-6, prev: float=0) -> float:
     """Newton method solver for 1 dimension, implemented recursively.
     
     Arguments:
@@ -14,16 +14,20 @@ def newtonSolver(f: Callable, f_prime: Callable, guess: float,
     
     Keyword Arguments:
         tol {float} -- Tolerance level (default: {10e-6}).
+        prev {float} -- Guess from previous iteration (for convergence check).
     
     Returns:
         float -- Solution to the function s.t. f(x) = 0.
     """
 
+    # Assigning current guess to x_old
     x_old = guess
 
-    if np.abs(f(x_old)) < tol:
+    # Checking if decision variable changed by less than tolerance level
+    if np.abs(x_old - prev) < tol:
         return x_old
     else:
+        # Compute new estimate for x
         x_new = x_old - (f(x_old) / f_prime(x_old))
-        return newtonSolver(f=f, f_prime=f_prime, guess=x_new, tol=tol)
-
+        return newtonSolver(f=f, f_prime=f_prime, guess=x_new,
+                            tol=tol, prev=x_old)
