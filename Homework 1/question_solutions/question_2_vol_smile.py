c731740f440d43f44a98aa734c53657407bfa0df
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-02-13 20:02:10 -0500

    Added logic to plot a 3D volatility surface for a given asset and option chain. Also made some minor tweaks to the 2D volatility smile plot (changes to title and axis labels - nothing major)

--- Homework 1/question_solutions/question_2_vol_smile.py
+++ Homework 1/question_solutions/question_2_vol_smile.py
@@ -1,6 +1,8 @@
 from context import fe621
 
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -10,13 +12,16 @@ spy_imp_vol = pd.read_csv('Homework 1/bin/spy_data1_vol.csv',
 amzn_imp_vol = pd.read_csv('Homework 1/bin/amzn_data1_vol.csv',
                            index_col=False, header=0)
 
+# Defining date of DATA1
+data1_date = '2019-02-06'
+
 
 def plot2DVolSmile(data: pd.DataFrame, name: str, save_loc: str):
     """Function to plot a 2D Volatility Smile for a given option chain.
     
     Arguments:
         data {pd.DataFrame} -- Input data containing implied volatilities.
-        name {str} -- Name of the asset.
+        name {str} -- Name of the underlying asset.
         save_loc {str} -- Location (folder) to save the output image.
     """
 
@@ -42,24 +47,94 @@ def plot2DVolSmile(data: pd.DataFrame, name: str, save_loc: str):
 
         ax = plt.gca()  # Get  current axes
 
-        # Setting y ticks
-        ax.set_yticklabels(['{:,.2%}'.format(i) for i in ax.get_yticks()])
-        # Setting x ticks
+        # Setting y ticks and label
+        ax.set_yticklabels(['{:,.1%}'.format(i) for i in ax.get_yticks()])
+        ax.set_ylabel('Implied Volatility')
+        # Setting x ticks and label
         ax.set_xticklabels(['$%i' % i for i in ax.get_xticks()])
+        ax.set_xlabel('Strike Price')
 
         # Setting legend and title
         plt.legend()
         full_option_type = 'Call' if (option_type == 'C') else 'Put'
-        plt.title(' '.join([name, full_option_type, 'Implied Volatility']))
+        plt.title(' '.join([name, full_option_type, 'Volatility Smile']))
 
+        # Saving to file
         fname = '_'.join([name, full_option_type, '2DVolSmile.png'])
         plt.savefig(fname=(save_loc + '/' + fname))
+
+        # Closing plot for next one
         plt.close()
 
 
+def plot3DVolatilitySurface(data: pd.DataFrame, name: str, save_loc: str):
+    """Fuction to plot a 3D Volatility Surface for a given option chain.
+    
+    Arguments:
+        data {pd.DataFrame} -- Input data containing implied volatilities
+        name {str} -- Name of the underlying asset.
+        save_loc {str} -- Location (folder) to save the output image.
+    """
+
+    # Iterating through types of options for 2 separate put/call imp vol plots
+    for option_type_group in data.groupby('type'):
+        # Isolating current option type
+        option_type = option_type_group[0]
+
+        # Isolating plot data
+        plot_data = option_type_group[1]
+
+        # Creating new column with time to maturity information for each option
+        ttm = plot_data.apply(lambda row: fe621.util.getTTM(
+                                name=row.loc['name'],
+                                current_date=data1_date),
+                              axis=1)
+        # Converting TTM to days
+        ttm_days = ttm * 365
+
+        # Isolating data for each axis
+        x = np.array(ttm_days)
+        y = np.array(plot_data['strike'])
+        z = np.array(plot_data['implied_vol'])
+
+        # Plotting surface
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.plot_trisurf(x, y, z, cmap='plasma')
+
+        # Formatting plot
+        #----------------
+        
+        # Setting x label
+        ax.set_xlabel('TTM (Days)')
+        # Setting y label
+        ax.set_ylabel('Strike Price ($)')
+        # Setting z label
+        ax.set_zlabel('Implied Volatility')
+
+        # Modifying z ticks to be percentages
+        ax.set_zticklabels(['{:,.0%}'.format(i) for i in ax.get_zticks()])
+
+        # Setting title
+        full_option_type = 'Call' if (option_type == 'C') else 'Put'
+        plt.title(' '.join([name, full_option_type, 'Volatility Surface']))
+
+        # Saving to file
+        fname = '_'.join([name, full_option_type, '3DVolSurface.png'])
+        plt.savefig(fname=(save_loc + '/' + fname))
+
+        # Closing plot for next one
+        plt.close()
+
 if __name__ == '__main__':
-    # Plotting 2D Volatility smile for AMZN and SPY option chains
+    # Plotting 2D Volatility Smile for AMZN and SPY option chains
     plot2DVolSmile(data=amzn_imp_vol, name='AMZN',
                    save_loc='Homework 1/bin/vol_smile/')
     plot2DVolSmile(data=spy_imp_vol, name='SPY',
                    save_loc='Homework 1/bin/vol_smile/')
+
+    # Plotting 3D Volatility Surface for AMZN and SPY option chains
+    plot3DVolatilitySurface(data=spy_imp_vol, name='SPY',
+                            save_loc='Homework 1/bin/vol_surface/')
+    plot3DVolatilitySurface(data=amzn_imp_vol, name='AMZN',
+                            save_loc='Homework 1/bin/vol_surface/')
