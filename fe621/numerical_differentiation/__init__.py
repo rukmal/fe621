50606c9aec0d540cc4459de81e4740f54f197ef3
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-02-17 17:35:55 -0500

    Adding submodule to encapsulate numerical integration. Added logic for estimation of the first derivative with the secant method

---
+++ fe621/numerical_differentiation/__init__.py
@@ -0,0 +1,1 @@
+from .first_derivative import firstDerivative
---
+++ fe621/numerical_differentiation/first_derivative.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+
+def firstDerivative(f: Callable, x: float, h: float=1e-7) -> float:
+    """Function to numerically approximate the first derivative about a point
+    `x`, given a function `f(x)` which takes a single float as its argument.
+    This function uses the secant method, computing the slope of a nearby secant
+    line passing through the points `(x - h)` and `(x + h)`.
+    
+    Arguments:
+        f {Callable} -- Objective function who's derivative is to be computed.
+        x {float} -- Point about which the derivative is computed.
+    
+    Keyword Arguments:
+        h {float} -- Step size (default: {1e-7}).
+    
+    Returns:
+        float -- Approximation of the first derivative of `f` about point `x`.
+    """
+
+    return (f(x + h) - f(x - h)) / (2 * h)
