508a6af9c756096a66b513aed95d425084ed1542
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-08 07:28:23 -0400

    Updated analytical greeks formulas to use dividend yield; added analytical formula for delta of a european put

--- fe621/black_scholes/greeks.py
+++ fe621/black_scholes/greeks.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def callDelta(current: float, volatility: float, ttm: float, strike: float,
-              rf: float) -> float:
+              rf: float, dividend: float=0) -> float:
     """Function to compute the Delta of a call option using the Black-Scholes
     formula.
     
@@ -17,13 +17,40 @@ def callDelta(current: float, volatility: float, ttm: float, strike: float,
         strike {float} -- Strike price of the option contract.
         rf {float} -- Risk-free rate (annual).
     
+    Keyword Arguments:
+        dividend {float} -- Dividend yield (annual) {default: {0}}.
+    
     Returns:
         float -- Delta of a European Call Option contract.
     """
 
     d1, _ = computeD1D2(current, volatility, ttm, strike, rf)
 
-    return norm.cdf(d1)
+    return np.exp(-1 * dividend * ttm) * norm.cdf(d1)
+
+
+def putDelta(current: float, volatility: float, ttm: float, strike: float,
+             rf: float, dividend: float=0) -> float:
+    """Function to compute the Delta of a put option using the Black-Scholes
+    formula.
+    
+    Arguments:
+        current {float} -- Current price of the underlying asset.
+        volatility {float} -- Volatility of the underlying asset price.
+        ttm {float} -- Time to expiration (in years).
+        strike {float} -- Strike price of the option contract.
+        rf {float} -- Risk-free rate(annual).
+    
+    Keyword Arguments:
+        dividend {float} -- Dividend yield (annual) (default: {0}).
+    
+    Returns:
+        float -- Delta of a European Put Option contract.
+    """
+
+    d1, _ = computeD1D2(current, volatility, ttm, strike, rf)
+
+    return -1 * np.exp(-1 * dividend * ttm) * norm.cdf(-1 * d1)
 
 
 def callGamma(current: float, volatility: float, ttm: float, strike: float,
