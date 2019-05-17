983c83ef6b75403dc938abecae993e09ec38d433
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-17 18:27:27 -0400

    Finished 3(b) implementation

--- Final Exam/question_solutions/question_3/question_3.py
+++ Final Exam/question_solutions/question_3/question_3.py
@@ -14,6 +14,9 @@ current_date = 39866
 
 # Part (a) Implied Volatility Computation
 def imp_vol_computation():
+    """3(a) [i] imp vol computation
+    """
+
     implied_vols = []
 
     for idx, row in main_data.iterrows():
@@ -50,6 +53,9 @@ def imp_vol_computation():
 
 
 def imp_vol_scatterplot():
+    """3(a) [ii] vol scatterplot
+    """
+
     # Loading data from CSV
     vol_data = pd.read_csv('Final Exam/bin/q3_imp_vols.csv')
 
@@ -80,8 +86,68 @@ def imp_vol_scatterplot():
     plt.close()
 
 
+def imp_vol_surface_plot():
+    """3(a) [ii] vol scatterplot
+    See: http://bit.ly/2WPOOID
+    """
+
+    # Loading data from CSV
+    vol_data = pd.read_csv('Final Exam/bin/q3_imp_vols.csv')
+    # Dropping rows with na data
+    vol_data = vol_data.dropna(axis=0)
+
+    # Importing matplotlib
+    # Doing this here so I can use debug in other methods
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    import matplotlib.pyplot as plt
+
+    # Scipy for cubic spline interpolation
+    import scipy
+    import scipy.interpolate
+
+    # Building grid of points
+    x_grid = np.linspace(min(vol_data['K']), max(vol_data['K']),
+                         len(vol_data['K']))
+    y_grid = np.linspace(min(vol_data['T']), max(vol_data['T']),
+                         len(vol_data['T']))
+    B1, B2 = np.meshgrid(x_grid, y_grid, indexing='xy')
+
+    # Interpolating with 2 dimensional cubic spline
+    cubic_spline = scipy.interpolate.griddata(
+        (vol_data['K'], vol_data['T']),
+        (vol_data['implied_vol']),
+        (B1, B2),
+        method='cubic'
+    )
+
+    # Getting figure and Axes3D
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # Plotting surface and points
+    ax.plot_surface(B1, B2, cubic_spline, alpha=0.5, rstride=1, cstride=1)
+    ax.scatter(vol_data['K'], vol_data['T'], vol_data['implied_vol'],
+               marker='x', c='red', alpha=0.2)
+
+    # Plot axes format
+    ax.set_xlabel('Strike ($)')
+    ax.set_ylabel('Time to Maturity')
+    ax.set_zlabel('Implied Volatility')
+
+    # Setting plot dimensions to tight
+    plt.tight_layout()
+
+    # Initializing view azimuth and elevation (trial + error)
+    ax.view_init(elev=35, azim=45)
+
+    # Saving figure
+    plt.savefig(fname='Final Exam/bin/q3_vol_surface.png')
+
 if __name__ == '__main__':
     # Part (a) [i] implied volatility computation
     # imp_vol_computation()
     # Part (a) [ii] implied volatility scatterplot
-    imp_vol_scatterplot()
+    # imp_vol_scatterplot()
+    # Part (b) Implied volatility surface
+    imp_vol_surface_plot()
