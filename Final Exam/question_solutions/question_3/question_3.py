976b28fe99cfb767ced0997d293d9ef7ef7f095e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-17 20:08:30 -0400

    Added solution for local volatility computation

--- Final Exam/question_solutions/question_3/question_3.py
+++ Final Exam/question_solutions/question_3/question_3.py
@@ -87,7 +87,7 @@ def imp_vol_scatterplot():
 
 
 def imp_vol_surface_plot():
-    """3(a) [ii] vol scatterplot
+    """3(b) [ii] vol surface plot
     See: http://bit.ly/2WPOOID
     """
 
@@ -144,10 +144,165 @@ def imp_vol_surface_plot():
     # Saving figure
     plt.savefig(fname='Final Exam/bin/q3_vol_surface.png')
 
+
+def calculate_local_vol():
+    # Loading data from CSV
+    vol_data = pd.read_csv('Final Exam/bin/q3_imp_vols.csv')
+    # Dropping rows with na data
+    vol_data = vol_data.dropna(axis=0)
+
+    # Building output dataframe
+    local_vols = []
+
+    # Iterating over rows
+    for idx, row in vol_data.iterrows():
+        dt = (1e-3 * row['T'])
+        dK = (1e-3 * row['K'])
+        d1, _ = fe621.black_scholes.util.computeD1D2(
+            current=current,
+            volatility=row['implied_vol'],
+            ttm=row['T'],
+            strike=row['K'],
+            rf=rf
+        )
+
+        # Function to get implied vol w.r.t. T, K, and Price
+        def impliedVol(T, K, price):    
+            # Defining function to be optimized
+            def optimFunc(x: float) -> float:
+                return fe621.black_scholes.call(
+                    current=current,
+                    volatility=x,
+                    ttm=T,
+                    strike=K,
+                    rf=rf
+                ) - price
+
+            # Running optimization
+            return fe621.optimization.bisectionSolver(
+                f=optimFunc,
+                a=0,
+                b=10
+            )
+        print('current ivol', row['implied_vol'])
+        fd_ivol_wrtT = lambda t: impliedVol(t, row['K'], row['Price'])
+        fd_ivol_wrtK = lambda k: impliedVol(row['T'], k, row['Price'])
+
+        try:
+            dSigdT = fe621.numerical_differentiation.firstDerivative(
+                f=fd_ivol_wrtT,
+                x=row['T'],
+                h=dt
+            )
+
+
+            dSigdK = fe621.numerical_differentiation.firstDerivative(
+                f=fd_ivol_wrtK,
+                x=row['K'],
+                h=dK
+            )
+
+            d2SigdK2 = fe621.numerical_differentiation.secondDerivative(
+                f=fd_ivol_wrtK,
+                x=row['K'],
+                h=dK
+            )
+        except Exception:
+            local_vols.append(np.nan)
+            continue
+
+        # Computing local volatility
+        sig = row['implied_vol']
+        K = row['K']
+        T = row['T']
+
+        # Computing local volatility
+        local_vol = np.sqrt(((2 * sig * dSigdT * T) + np.power(sig, 2) +
+            (2 * sig * rf * T * K * dSigdK)) / (np.power(
+            (1 + (K * d1 * dSigdK * np.sqrt(T))), 2) + (np.power(K, 2) * T * sig
+            * (d2SigdK2 - (d1 * np.power(dSigdK, 2) * np.sqrt(T))))))
+        print(local_vol)
+
+        # Appending to outout array
+        local_vols.append(local_vol)
+
+
+    # Adding local volatility to the dataframe
+    vol_data['local_vol'] = local_vols
+
+    # Saving as new csv
+    vol_data.to_csv('Final Exam/bin/q3_local_vol.csv')
+
+def local_vol_surface_plot():
+    """3(d) [ii] vol surface
+    See: http://bit.ly/2WPOOID
+    """
+
+    # Loading data from CSV
+    vol_data = pd.read_csv('Final Exam/bin/q3_local_vols.csv')
+    # Dropping rows with na data
+    vol_data = vol_data.dropna(axis=0)
+
+    # Importing matplotlib
+    # Doing this here so I can use debug in other methods
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    import matplotlib.pyplot as plt
+
+    # Scipy for cubic spline interpolation
+    import scipy
+    import scipy.interpolate
+
+    # Building grid of points
+    x_grid = np.linspace(min(vol_data['K']), max(vol_data['K']),
+                         len(vol_data['K']))
+    y_grid = np.linspace(min(vol_data['T']), max(vol_data['T']),
+                         len(vol_data['T']))
+    B1, B2 = np.meshgrid(x_grid, y_grid, indexing='xy')
+
+    # Interpolating with 2 dimensional cubic spline
+    cubic_spline = scipy.interpolate.griddata(
+        (vol_data['K'], vol_data['T']),
+        (vol_data['local_vol']),
+        (B1, B2),
+        method='cubic'
+    )
+
+    # Getting figure and Axes3D
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # Plotting surface and points
+    ax.plot_surface(B1, B2, cubic_spline, alpha=0.5, rstride=1, cstride=1)
+    ax.scatter(vol_data['K'], vol_data['T'], vol_data['local_vol'],
+               marker='x', c='red', alpha=0.2)
+
+    # Plot axes format
+    ax.set_xlabel('Strike ($)')
+    ax.set_ylabel('Time to Maturity')
+    ax.set_zlabel('Local Volatility')
+
+    # Setting plot dimensions to tight
+    plt.tight_layout()
+
+    # Initializing view azimuth and elevation (trial + error)
+    ax.view_init(elev=35, azim=45)
+
+    # Saving figure
+    plt.savefig(fname='Final Exam/bin/q3_local_vol_surface.png')
+
 if __name__ == '__main__':
     # Part (a) [i] implied volatility computation
     # imp_vol_computation()
+
     # Part (a) [ii] implied volatility scatterplot
     # imp_vol_scatterplot()
+
     # Part (b) Implied volatility surface
-    imp_vol_surface_plot()
+    # imp_vol_surface_plot()
+
+    # Part (d) Dupire's Local Vol
+    calculate_local_vol()
+
+    # Part (d) Cont. Dupire's Local Vol Plot
+    local_vol_surface_plot()
