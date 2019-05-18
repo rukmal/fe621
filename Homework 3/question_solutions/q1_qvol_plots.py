4e3aa418bcaa6e16e661bd629a301daadc3744e3
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-18 06:48:12 -0400

    Added q1 bonus

--- Homework 3/question_solutions/q1_qvol_plots.py
+++ Homework 3/question_solutions/q1_qvol_plots.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from typing import Callable
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 # Setting parameters
@@ -54,6 +55,8 @@ x, x0 = np.meshgrid(x, x0)
 # Defining vector of t's for plotting
 t_vec = np.arange(10, 41, 10)
 
+# Part (a) Quadratic Vol Plots
+
 for t in t_vec:
     fig = plt.figure()
     ax = fig.gca(projection='3d')
@@ -62,3 +65,33 @@ for t in t_vec:
     plt.tight_layout()
     plt.savefig(fname='Homework 3/bin/q1_quadvol_t_{0}.png'.format(t))
     plt.close()
+
+
+# Part (b)
+
+# Verifying that the finite difference approxiumations of the transition
+# probability density satisfies the PDE
+
+# Partial of density w.r.t. time
+def partialT(x, x0, t, delT):
+    return (probability(x, x0, t + delT) - probability(x, x0, t)) / delT
+
+# Partial of density w.r.t. price
+def partialX(x, x0, t, delX):
+    return (probability(x + delX, x0, t) - probability(x, x0, t)) / delX
+
+
+delX = delT = 1e-3
+x = 50
+x0 = 40
+t = 20
+
+# Computing difference
+diff = np.abs(partialT(x, x0, t, delT) - (np.power(sigma(x), 2) * 0.5 *
+    partialX(x, x0, t, delX)))
+
+# Saving to CSV file
+pd.DataFrame({
+    'Absolute Difference between PDE and Finite Difference Approximation': \
+        [diff]
+}).to_csv('Homework 3/bin/q1_finite_diff_approx_verification.csv', index=False)
